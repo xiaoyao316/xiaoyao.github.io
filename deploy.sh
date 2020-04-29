a876d8cbee06152994c87169f020f6b455274099
xiaoyao <xiaoy@dachentech.com.cn>
2020-04-29 19:32:54 +0800

    调试自动构建

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+npm run build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
@@ -17,9 +17,12 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:xiaoyao316/xiaoyao316.github.io.git master
+# git push -f git@github.com:xiaoyao316/xiaoyao316.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# git push -f git@github.com:xiaoyao316/xiaoyao316.github.io.git master
+
+# Travis配置的推送
+git push -f https://5d5b68068bbf89b5a21f8075dc7827c4f14cf847@github.com/xiaoyao316.github.io.git master:master
 
 cd -
